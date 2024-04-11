eb5bc2e938be103aa340184dd95ad339b2ad949a
Krushna Chandra <76234660+developer-krushna@users.noreply.github.com>
2024-04-11 15:05:39 +0530

    Update Notify_MT.java
    * Code enhancement

--- app/src/main/java/modder/hub/dexeditor/Notify_MT.java
+++ app/src/main/java/modder/hub/dexeditor/Notify_MT.java
@@ -1,37 +1,37 @@
 /*
- * Dex-Editor-Android an Advanced Dex Editor for Android 
- * Copyright 2024, developer-krushna
- *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions are
- * met:
- *
- *     * Redistributions of source code must retain the above copyright
- * notice, this list of conditions and the following disclaimer.
- *     * Redistributions in binary form must reproduce the above
- * copyright notice, this list of conditions and the following disclaimer
- * in the documentation and/or other materials provided with the
- * distribution.
- *     * Neither the name of developer-krushna nor the names of its
- * contributors may be used to endorse or promote products derived from
- * this software without specific prior written permission.
- *
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
- * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
- * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
- * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
- * OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
- * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
- * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
- * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
- * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
- * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
- * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- 
- 
- *     Please contact Krushna by email mt.modder.hub@gmail.com if you need
- *     additional information or have any questions
- */
+* Dex-Editor-Android an Advanced Dex Editor for Android 
+* Copyright 2024, developer-krushna
+*
+* Redistribution and use in source and binary forms, with or without
+* modification, are permitted provided that the following conditions are
+* met:
+*
+*     * Redistributions of source code must retain the above copyright
+* notice, this list of conditions and the following disclaimer.
+*     * Redistributions in binary form must reproduce the above
+* copyright notice, this list of conditions and the following disclaimer
+* in the documentation and/or other materials provided with the
+* distribution.
+*     * Neither the name of developer-krushna nor the names of its
+* contributors may be used to endorse or promote products derived from
+* this software without specific prior written permission.
+*
+* THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+* "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+* LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+* A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+* OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+* SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+* LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+* DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+* THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+* (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+* OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+
+*     Please contact Krushna by email mt.modder.hub@gmail.com if you need
+*     additional information or have any questions
+*/
 
 
 
@@ -95,6 +95,8 @@ Author @developer-krushna
 
 public class Notify_MT {
 	
+	public static int cornerRadius = 20;
+	
 	
 	public static void Notify(Context context, String title_mt, String message_mt, String cancel_mt) {
 		try {
@@ -109,13 +111,8 @@ public class Notify_MT {
 			});
 			
 			// Setting custom background
-			int cornerRadius = 20;
-			android.graphics.drawable.GradientDrawable gd = new android.graphics.drawable.GradientDrawable();
-			gd.setColor(Color.parseColor("#FFFFFF"));
-			gd.setCornerRadius(cornerRadius);
-			
 			final AlertDialog alert = builder.create();
-			alert.getWindow().setBackgroundDrawable(gd);
+			alert.getWindow().setBackgroundDrawable(createDrawable(cornerRadius, 0xFFFFFFFF));
 			alert.show();
 			final TextView message = alert.findViewById(android.R.id.message);
 			message.setTextIsSelectable(true);
@@ -126,14 +123,8 @@ public class Notify_MT {
 	
 	
 	public static void Dlg_Style(AlertDialog.Builder dialog){
-		
-		int cornerRadius = 20;
-		android.graphics.drawable.GradientDrawable gd = new android.graphics.                      
-		drawable.GradientDrawable();
-		gd.setColor(Color.parseColor("#FFFFFF"));
-		gd.setCornerRadius(cornerRadius);
 		final AlertDialog alert = dialog.create();
-		alert.getWindow().setBackgroundDrawable(gd);
+		alert.getWindow().setBackgroundDrawable(createDrawable(cornerRadius, 0xFFFFFFFF));
 		try{
 			alert.show();
 		} catch (WindowManager.BadTokenException e){
@@ -141,10 +132,12 @@ public class Notify_MT {
 		}
 		
 	}
-	public static void mt(View _mt){
-		_mt.setVisibility(View.VISIBLE);
-		
-		
-		
+	public static GradientDrawable createDrawable(int cornerRadiuss, int color) {
+		GradientDrawable drawable = new GradientDrawable();
+		drawable.setCornerRadius(cornerRadiuss);
+		drawable.setColor(color);
+		return drawable;
 	}
+	
+	
 }
