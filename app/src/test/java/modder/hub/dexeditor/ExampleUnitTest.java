fa157880854323493f731413b4f0655967b1bd40
Krushna Chandra <mt.modder.hub@gmail.com>
2026-05-23 22:52:02 +0530

    Create ExampleUnitTest.java

---
+++ app/src/test/java/modder/hub/dexeditor/ExampleUnitTest.java
@@ -0,0 +1,11 @@
+package modder.hub.dexeditor;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class ExampleUnitTest {
+    @Test
+    public void addition_isCorrect() {
+        assertEquals(4, 2 + 2);
+    }
+}
